aaefb7e01a1f65b1010968edb24c49de800186f4
Viktorvitols <viktorvitols@gmail.com>
2021-01-27 12:31:43 +0200

    Ticket create form HTML+CSS+JS

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,11 @@
+var modal = document.getElementById("createTask");
+var btn = document.getElementById("taskBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
