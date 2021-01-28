082b2e8b602f028dbe1d580a43abff5d9e47a28e
Viktorvitols <viktorvitols@gmail.com>
2021-01-28 22:30:13 +0200

    Validation added. Failed validation page added.

--- src/main/java/com/app/controllers/TestFormController.java
+++ src/main/java/com/app/controllers/TestFormController.java
@@ -1,6 +1,8 @@
 package com.app.controllers;
 
 import com.app.model.TaskForm;
+import com.app.services.TaskFormService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class TestFormController {
 
+    @Autowired
+    private TaskFormService taskFormService;
+
     @GetMapping("/taskform")
     public String getTaskForm(Model model) {
         model.addAttribute("taskform", new TaskForm());
@@ -17,13 +22,22 @@ public class TestFormController {
     }
 
     @GetMapping("/success")
-    public String getTaskForm() {
+    public String getSuccessForm() {
         return "success";
     }
 
+    @GetMapping("/invalid")
+    public String getInvalidForm() {
+        return "invalid";
+    }
+
+
     @PostMapping("/taskform")
     public String submitNewTask(@ModelAttribute TaskForm taskForm, Model model) {
-        model.addAttribute("taskData", taskForm);
-        return "redirect:/success";
+        model.addAttribute("taskform", taskForm);
+        if (taskFormService.validateTaskData(taskForm) == true) {
+            return "redirect:/success";
+        }
+        return "redirect:/invalid";
     }
 }
---
+++ src/main/java/com/app/services/TaskFormService.java
@@ -0,0 +1,34 @@
+package com.app.services;
+
+import com.app.model.TaskForm;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskFormService {
+
+    public boolean validateTaskData(TaskForm taskForm) {
+//        taskForm = new TaskForm();
+        boolean isValid = true;
+
+        while (isValid = true) {
+            if (taskForm.getProject().isEmpty()) {
+                isValid = false;
+            }
+
+            if (taskForm.getSummary().isEmpty()) {
+                isValid = false;
+            }
+
+            if (taskForm.getSummary().length() > 100) {
+                isValid = false;
+            }
+
+            if (taskForm.getDescription().length() > 4000) {
+                isValid = false;
+            }
+
+            break;
+        }
+        return isValid;
+    }
+}
