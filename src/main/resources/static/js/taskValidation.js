aefed48ba2583bb0a26f4a6b1265a76b1b5358b2
Viktorvitols <viktorvitols@gmail.com>
2021-02-03 11:36:55 +0200

    Task form validation dont work

---
+++ src/main/resources/static/js/taskValidation.js
@@ -0,0 +1,19 @@
+$().ready(function () {
+
+    $('#taskform').validate({
+        rules: {
+            project: {
+                required: true,
+                maxlength: 5
+            },
+            summary: {
+                required: true,
+                minlength: 5
+            },
+            description: {
+                maxlength: 4000
+             }
+        }
+    });
+
+});
